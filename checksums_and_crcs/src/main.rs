6a7a996caf0b2d7fa5c8e53500c714f7b37a3b70
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-04-28 21:45:31 +0100

    add calculation

--- checksums_and_crcs/src/main.rs
+++ checksums_and_crcs/src/main.rs
@@ -1,8 +1,18 @@
 mod checksums {
     pub fn is_checksum_valid(f: u64) -> bool {
-        let (dev, m) = calculate_mod(f);
+        let (f, check_value) = calculate_mod(f);
+        let mut checksum = 0;
+        let mut value_to_checksum = f;
 
-        if f > 0 {
+        while value_to_checksum > 0 {
+            let (new_value, m) = calculate_mod(value_to_checksum);
+            checksum += m;
+            value_to_checksum = new_value;
+        }
+
+        checksum %= 10;
+
+        if check_value == checksum {
             return true;
         }
         
@@ -12,9 +22,9 @@ mod checksums {
     fn calculate_mod(f: u64) -> (u64, u64) {
         
         if f == 0 {
-            return(0,0);
+            return (0,0);
         } else {
-            return (f>>10, f%10);
+            return (f/10, f%10);
         }
     }
 }
@@ -22,5 +32,11 @@ mod checksums {
 
 fn main() {
 
-    let c = checksums::is_checksum_valid(0);
+    let correct_value = 41255501238;
+
+    let is_number_valid = checksums::is_checksum_valid(correct_value);
+
+    println!("The number {} has a crc valid == {}", correct_value, is_number_valid);
+
+    
 }
